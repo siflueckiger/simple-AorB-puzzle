64f9fc8ba04cb04ae170b9e7581be30bb6eac9a1
seamon <si.flueckiger@gmail.com>
2023-04-09 10:28:50 +0200

    add audio

--- main.ts
+++ main.ts
@@ -1,8 +1,10 @@
 input.onButtonPressed(Button.AB, function () {
     basic.clearScreen()
+    basic.pause(200)
     game_state = "running"
     timer_seconds = 0
     timer_rounds = 0
+    level = 0
 })
 function wait_animation () {
     basic.showLeds(`
@@ -26,6 +28,15 @@ function countdown_timer () {
     timer_seconds = 0
     for (let index = 0; index < 5; index++) {
         led.toggle(timer_seconds, 0)
+        if (level == 0) {
+            music.playMelody("C5 - - - - - - - ", 800)
+        } else if (level == 1) {
+            music.playMelody("C5 - C5 - - - - - ", 800)
+        } else if (level == 2) {
+            music.playMelody("C5 - C5 - C5 - - - ", 800)
+        } else {
+        	
+        }
         basic.pause(GAME_SPEED)
         timer_seconds += 1
     }
@@ -35,15 +46,14 @@ function countdown_timer () {
         game_state = "game_over"
     }
 }
-let MAX_ROUNDS = 0
-let GAME_SPEED = 0
+let level = 0
 let timer_rounds = 0
 let timer_seconds = 0
+let MAX_ROUNDS = 0
+let GAME_SPEED = 0
 let game_state = ""
 game_state = "wait_for_start"
-timer_seconds = 0
-timer_rounds = 0
-GAME_SPEED = 750
+GAME_SPEED = 5
 MAX_ROUNDS = 5
 control.inBackground(function () {
     while (true) {
@@ -53,6 +63,10 @@ control.inBackground(function () {
             countdown_timer()
         } else if (game_state == "game_over") {
             basic.showIcon(IconNames.Skull)
+            music.setTempo(120)
+            music.playSoundEffect(music.builtinSoundEffect(soundExpression.sad), SoundExpressionPlayMode.UntilDone)
+            game_state = "wait_for_start"
+            basic.pause(1000)
         } else {
         	
         }
